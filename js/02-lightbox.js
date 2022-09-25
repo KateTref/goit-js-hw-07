d8a03f304a5ff8261ac5b30cda21c878ba1b8eac
KateTref <trefilovaekaterina408@gmail.com>
2022-09-25 18:20:21 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const picturesMarkup = createPicturesMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', picturesMarkup);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+
+function createPicturesMarkup(Items) {
+    return Items
+        .map(({ preview, original, description }) => {
+            return `
+       <li class="galllery__item> <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>
+`}).join('');
+};
+
+ const lightbox = new SimpleLightbox('.gallery li', { captionsData: "alt", captionsDelay: 250 });
+
+function onGalleryContainerClick(event) {
+    event.preventDefault();
+    
+    const isPictureEl = event.target.classList.contains('gallery__image');
+
+    if (!isPictureEl) {
+        return;
+    };
+};
