8dea3c5ab45e8c8da98388e6d375af4bccbda1d1
KateTref <trefilovaekaterina408@gmail.com>
2022-09-29 01:15:00 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,14 +4,12 @@ const galleryContainer = document.querySelector('.gallery');
 const picturesMarkup = createPicturesMarkup(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', picturesMarkup);
-galleryContainer.addEventListener('click', onGalleryContainerClick);
-
 
 function createPicturesMarkup(Items) {
     return Items
         .map(({ preview, original, description }) => {
             return `
-       <li class="galllery__item>
+       <li class="gallery__item>
        <a class="gallery__link" href="${original}">
        <img class="gallery__image" src="${preview}" alt="${description}" />
        </a>
@@ -21,12 +19,3 @@ function createPicturesMarkup(Items) {
 
  const lightbox = new SimpleLightbox('.gallery li', { captionsData: "alt", captionsDelay: 250 });
 
-function onGalleryContainerClick(event) {
-    event.preventDefault();
-    
-    const isPictureEl = event.target.classList.contains('gallery__image');
-
-    if (!isPictureEl) {
-        return;
-    };
-};
