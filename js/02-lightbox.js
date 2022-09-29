694c982c672998fad15d86fb0f447d03392e8d86
KateTref <trefilovaekaterina408@gmail.com>
2022-09-29 19:50:18 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,7 +9,7 @@ function createPicturesMarkup(Items) {
     return Items
         .map(({ preview, original, description }) => {
             return `
-       <li class="gallery__item>
+       <li class="gallery__item">
        <a class="gallery__link" href="${original}">
        <img class="gallery__image" src="${preview}" alt="${description}" />
        </a>
@@ -17,5 +17,5 @@ function createPicturesMarkup(Items) {
 `}).join('');
 };
 
- const lightbox = new SimpleLightbox('.gallery li', { captionsData: "alt", captionsDelay: 250 });
+ const lightbox = new SimpleLightbox('.gallery__item a', { captionsData: "alt", captionsDelay: 250 });
 
